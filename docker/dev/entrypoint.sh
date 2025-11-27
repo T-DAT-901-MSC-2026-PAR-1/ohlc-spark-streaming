e6d9c2f888ab6374aa04f1623b976d95c56358d3
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-11-27 17:10:26 +0100

    docker: fix the spark setup

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -20,18 +20,14 @@ if [ "$#" -gt 0 ]; then
 fi
 
 # Construct spark-submit command
+# In client deploy mode, the driver runs in this container but tasks execute on workers.
+# Workers need access to Python files. With --py-files we can distribute the main script.
+# For a single-file app, we copy main.py or use local[*] mode for testing.
+# Note: spark-sql-kafka doesn't require executor-side code for most streaming operations.
 SPARK_CMD=(/opt/spark/bin/spark-submit
   --master "${SPARK_MASTER_URL}"
   --deploy-mode client
   --conf "spark.jars.packages=org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1"
-  --conf "spark.executorEnv.KAFKA_BOOTSTRAP_SERVERS=${KAFKA_BOOTSTRAP_SERVERS}"
-  --conf "spark.executorEnv.KAFKA_SUBSCRIBE_TOPICS=${KAFKA_SUBSCRIBE_TOPICS}"
-  --conf "spark.executorEnv.KAFKA_OUTPUT_PREFIX=${KAFKA_OUTPUT_PREFIX}"
-  --conf "spark.executorEnv.CHECKPOINT_LOCATION=${CHECKPOINT_LOCATION}"
-  --conf "spark.executorEnv.WINDOW_DURATION=${WINDOW_DURATION}"
-  --conf "spark.executorEnv.WATERMARK_DELAY=${WATERMARK_DELAY}"
-  --conf "spark.executorEnv.SPARK_APP_NAME=${SPARK_APP_NAME}"
-  --conf "spark.executorEnv.SPARK_MASTER_URL=${SPARK_MASTER_URL}"
   /opt/app/main.py
 )
 
